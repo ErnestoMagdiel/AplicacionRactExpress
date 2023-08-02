975d2853dab689c26605ffe292a7b283a9c123d9
Ernesto Magdiel <ernestomagdiel1996@gmail.com>
2023-08-02 10:41:22 -0300

    app mixta

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require ("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.listen (PORT,()=> console.log ("Server started"));
+
+app.use(express.static("dist"));
+
+const items =[
+  {name:"Laptop",
+   price:"500",},
+   {
+    name:"Desktop",
+    price:"700",
+   },
+];
+
+app.get("/api/items", (req,res)=>{
+  res.send(items);
+});
